2f6273366d97e97ceb76fecd4d4caf0c31307a88
sj-shoff <atryakhin0306@gmail.com>
2024-11-15 21:42:26 +0300

    modified:   go.mod 	modified:   go.sum 	deleted:    not_used.go 	new file:   struct.go

---
+++ struct.go
@@ -0,0 +1,42 @@
+package main
+
+func main() {
+
+}
+
+type Person_data struct {
+	ID           uint     `gorm:"primaryKey"`
+	Full_name    string   `gorm:"not null"`
+	DateOfBirth  string   `gorm:"not null"`
+	Mobile_phone uint     `gorm:"not null"`
+	Pasport      string   `gorm:"not null"`
+	Pasport_data []string `gorm:"many"`
+	Licence      []string `gorm:"many"`
+	Registration string   `gorm:"many"`
+	Polis        uint     `gorm:"not null"`
+	TimeOfUsing  uint     `gorm:"not null"`
+}
+
+type Car struct {
+	ID          uint          `gorm:"primaryKey"`
+	Based_info  []Person_data `gorm:"many"`
+	Mark        string        `gorm:"not null"`
+	Model       string        `gorm:"not null"`
+	Year        uint          `gorm:"not null"`
+	Reg_Number  uint          `gorm:"not null"`
+	Category    string        `gorm:"not null"`
+	Vin         uint          `gorm:"not null"`
+	Sts_reg     uint          `gorm:"not null"`
+	Engine_info []string      `gorm:"many"`
+}
+
+type Health struct {
+	ID           uint     `gorm:"primaryKey"`
+	Full_name    string   `gorm:"not null"`
+	DateOfBirth  string   `gorm:"not null"`
+	Mobile_phone uint     `gorm:"not null"`
+	Pasport      string   `gorm:"not null"`
+	Pasport_data []string `gorm:"many"`
+	Licence      []string `gorm:"many"`
+	Registration string   `gorm:"many"`
+}
