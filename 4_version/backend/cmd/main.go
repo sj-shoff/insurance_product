c33df1c510f914853a11eeb22949336b7dc45f8d
sj-shoff <atryakhin0306@gmail.com>
2024-11-17 09:04:36 +0300

    modified:   4_version/backend/cmd/main.go

--- 4_version/backend/cmd/main.go
+++ 4_version/backend/cmd/main.go
@@ -9,11 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const (
-	PORT      = 3000
-	DATA_FILE = "data.json"
-)
-
 var db *sql.DB
 
 func main() {
@@ -29,25 +24,22 @@ func main() {
 	}
 
 	r := gin.Default()
-	// Убедитесь, что путь к шаблонам правильный
 	r.LoadHTMLGlob("4_version/reg/templates/*")
 
 	r.GET("/", server.ShowHomePage)
 	r.GET("/register", server.ShowRegistrationForm)
 	r.POST("/register", server.RegisterUser)
+	r.POST("/login", server.Login())
 
 	// Маршрут для сохранения данных о продукте
 	r.POST("/save", server.SaveProductHandler(db))
 
-	//r.GET("/login", server.ShowLoginForm)
-	r.POST("/login", server.Login())
-
-	// Запуск сервера
-	if err := r.Run(":3000"); err != nil {
+	// Запуск сервера на порту 8080
+	if err := r.Run(":8080"); err != nil {
 		log.Fatal("Ошибка при запуске сервера: ", err)
 	}
 
-	log.Println("Сервер запущен на http://localhost:3000")
+	log.Println("Сервер запущен на http://localhost:8080")
 }
 
 //запускать через  go run 4_version/backend/cmd/main.go
